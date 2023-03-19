eb7340d2c5aebd30ce173a7090c8036b06c19993
Alexandr Ivanov <arlert@mail.ru>
2023-03-19 17:31:59 +0200

    Add perspective map

--- headunit/tools/openlayers/src/map.js
+++ headunit/tools/openlayers/src/map.js
@@ -1,4 +1,4 @@
-import { View } from 'ol';
+import { Feature, Map, View } from 'ol';
 import { VectorTile as VectorTileSource } from 'ol/source';
 import { VectorTile as VectorTileLayer } from 'ol/layer';
 import { MVT } from 'ol/format';
@@ -6,6 +6,11 @@ import {useGeographic} from 'ol/proj.js';
 import PerspectiveMap from 'ol-ext/map/PerspectiveMap'
 import {applyStyle, stylefunction} from 'ol-mapbox-style';
 import style from './style.js';
+import Style from 'ol/style/Style.js';
+import Icon from 'ol/style/Icon.js';
+import Point from 'ol/geom/Point.js';
+import VectorSource from 'ol/source/Vector.js';
+import VectorLayer from 'ol/layer/Vector.js';
 
 function initMap() {
 
@@ -26,17 +31,41 @@ function initMap() {
         source: source,
     });
     
-    var map = new PerspectiveMap({
-      allOverlays: false,
-        layers: [ layer ],
-        target: 'map',
-        view: new View({
-            center: [28.833677,47.023006],
-            zoom: 14,
-            rotation: Math.PI / 6
+    var iconStyle = new Style({
+        image: new Icon({
+            src: 'image.png',
+            rotation: Math.PI / 3,
+            rotateWithView: true,
+            width: 48,
+            height: 48
         })
     });
+    var iconFeature = new Feature({
+        geometry: new Point([28.832178,46.995910])
+    });
+    iconFeature.setStyle(iconStyle);
+    
+    var vectorSource = new VectorSource({
+        features: [iconFeature]
+    });
+    var vectorLayer = new VectorLayer({
+        source: vectorSource
+    });
+
+    var view = new View({
+        center: [28.833677,47.023006],
+        zoom: 18, 
+        rotation: -Math.PI / 3
+    });
+    var map = new PerspectiveMap({
+      allOverlays: false,
+        layers: [ layer, vectorLayer ],
+        target: 'map',
+        view: view
+    });
     map.on('precompose', function() { 
+        console.log(view.getCenter());
+        console.log(map.getSize());
         console.log('precompose', Date.now()); 
     });
     map.on('rendercomplete', function() { 
@@ -45,6 +74,10 @@ function initMap() {
     map.setPerspective(45);
     
     applyStyle(layer, style);
+
+    var size = map.getSize();
+    view.centerOn(iconFeature.getGeometry().getCoordinates(), size, [395 * 2 + 6, 405 * 2 + 40]);
+
     //stylefunction(layer, style, 'openlayers');
     // applyStyle(layer, style, 'openlayers');
 }
